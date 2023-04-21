02fb3facfe84d71e76d2b6d18c238238514d13a7
Lisa Dafny <lisadafny.tech@gmail.com>
2023-04-21 14:19:32 -0300

    feature: white pawns chess rules

--- chess.js
+++ chess.js
@@ -1,23 +1,56 @@
+let allChessSquares = '';
+let allChessWhitePieces = $('.chess-piece.white');
+let allChessBlackPieces = $('.chess-piece.black');
+const allChessPieces = $('.chess-piece');
+const arrCol = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+
 $(document).ready(function () {
     createChessBoard();
     insertChessPieces();
     dragChessPieces();
     dropChessPieces();
+    $('.fa-chess-pawn').addClass('first-move');
+    allChessSquares = $('.chess-square');
+    allChessWhitePieces = $('.chess-piece.white');
+    allChessBlackPieces = $('.chess-piece.black');
+    allChessWhitePieces.on('click', movableAreas);
+    allChessWhitePieces.addClass('can-move');
 });
 
-function createChessBoard(){
-    let row = '';
-    for(let i = 0; i < 64; i++){
-        if((7 < i && i < 16) || (23 < i && i < 32) || (39 < i && i < 48) || 55 < i)
-            row = 'row-opposite-color';
-        else
-            row = 'row-normal-color';
+function createChessBoard() {
+    let position = '';
 
-        $('#chessBoard').append(`<div class="chess-square ${row}"></div>`);
+    for (let i = 0; i < 64; i++) {
+
+        if (i < 8)
+            position = `row-8 col-${arrCol[i]}`;
+
+        if (8 <= i && i < 16)
+            position = `row-7 col-${arrCol[i - 8]}`;
+
+        if (16 <= i && i < 24)
+            position = `row-6 col-${arrCol[i - 16]}`;
+
+        if (24 <= i && i < 32)
+            position = `row-5 col-${arrCol[i - 24]}`;
+
+        if (32 <= i && i < 40)
+            position = `row-4 col-${arrCol[i - 32]}`;
+
+        if (40 <= i && i < 48)
+            position = `row-3 col-${arrCol[i - 40]}`;
+
+        if (48 <= i && i < 56)
+            position = `row-2 col-${arrCol[i - 48]}`;
+
+        if (56 <= i)
+            position = `row-1 col-${arrCol[i - 56]}`;
+
+        $('#chessBoard').append(`<div class="chess-square ${position}"></div>`);
     }
 }
 
-function insertChessPieces(){
+function insertChessPieces() {
 
     const rookB = `<i class="fa-solid fa-chess-rook chess-piece black fa-3x"></i>`;
     const knightB = `<i class="fa-solid fa-chess-knight chess-piece black fa-3x"></i>`;
@@ -68,24 +101,99 @@ function insertChessPieces(){
     $('.chess-square:eq(48)').append(pawnW);
 }
 
-function dragChessPieces(){
+function dragChessPieces() {
     $('.chess-piece').draggable({
         containment: "#chessBoard",
         cursor: "move", cursorAt: { top: 24, left: 15 },
-        drag: function (event, ui){
+        drag: function (event, ui) {
             $(event.target).css('position', 'absolute');
         }
     });
 }
 
-function dropChessPieces(){
+function dropChessPieces() {
     $(".chess-square").droppable({
         drop: function (event, ui) {
             let taskCard = ui.helper;
             let section = event.target;
+            scoreMaker($(section).children());
             $(section).children().remove();
             $(taskCard).appendTo($(section));
             $(taskCard).css('position', 'static');
         }
     });
+}
+
+function movableAreas(event) {
+    const piece = $(event.target);
+    const chessSquare = piece.parent();
+    chessSquare.addClass('selected');
+    const classes = chessSquare.attr('class').split(' ');
+    const file = classes.filter((x) => x.startsWith('col'))[0];
+    const rank = classes.filter((x) => x.startsWith('row'))[0];
+    const isPawn = piece.hasClass('fa-chess-pawn');
+    allChessSquares.on('click', (event) => pieceToMove(event, piece));
+    event.stopPropagation();
+    if (isPawn) {
+        movePawn(piece, rank, file);
+        return;
+    }
+}
+
+function movePawn(piece, rank, file) {
+    const rowPosition = + rank.charAt(4);
+    const colPosition = file.charAt(4);
+    const colIndex = arrCol.indexOf(colPosition);
+    const isFirstMove = piece.hasClass('first-move');
+    const isWhite = piece.hasClass('white');
+    if (isWhite) {
+        const oneUp = $(`.${file}.row-${rowPosition + 1}`);
+        const twoUp = $(`.${file}.row-${rowPosition + 2}`);
+        const diagonalLeft = $(`.col-${arrCol[colIndex - 1]}.row-${rowPosition + 1}`);
+        const diagonalright = $(`.col-${arrCol[colIndex + 1]}.row-${rowPosition + 1}`);
+        console.log(oneUp);
+        console.log(twoUp);
+        if (!oneUp.children().length)
+            oneUp.addClass('can-move');
+        if (isFirstMove && !twoUp.children().length)
+            twoUp.addClass('can-move');
+        if (diagonalLeft.children().length)
+            diagonalLeft.addClass('can-move');
+        if (diagonalright.children().length)
+            diagonalright.addClass('can-move');
+    }
+}
+function scoreMaker(piece) {
+    console.log(piece);
+    console.log("not implement yet");
+}
+function pieceToMove(event, element) {
+    const isWhite = $(element).hasClass('white');
+    const chessSquare = $(event.target);
+    const selectedChessSquare = $('.chess-square.selected');
+    const canMoveChessSquare = $('.chess-square.can-move');
+    const pieceAtSquare = chessSquare.children();
+    let boolMoved = false;
+    canMove = chessSquare.hasClass('can-move');
+    if (canMove) {
+        if (pieceAtSquare.length) {
+            scoreMaker(pieceAtSquare);
+            pieceAtSquare.remove();
+        }
+        chessSquare.append(element);
+        element.removeClass('first-move');
+        boolMoved = true;
+    }
+    canMoveChessSquare.removeClass('can-move');
+    selectedChessSquare.removeClass('selected');
+    if (!boolMoved)
+        return;
+    allChessSquares.off('click', '**');
+    if (isWhite) {
+        // allChessWhitePieces.off('click', '**');
+        // allChessBlackPieces.on('click', movableAreas);
+        return;
+    }
+    allChessBlackPieces.off('click', '**');
+    allChessWhitePieces.on('click', movableAreas);
 }
